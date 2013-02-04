a695eff51e6c8d6494a55349dfd2a11e67117437
svedrenne <sylvain@jankenpoi.net>
2013-02-04 16:50:37 +0100

    Commit modifications from version 1.1.7.

--- src/classes/net/jankenpoi/sudokuki/Version.java
+++ src/classes/net/jankenpoi/sudokuki/Version.java
@@ -7,6 +7,6 @@ package net.jankenpoi.sudokuki;
 
 public interface Version {
 
-    public String versionString = "Sudokuki 1.1.6";
+    public String versionString = "Sudokuki 1.1.7";
 
 }
--- sudokuki.sh
+++ sudokuki.sh
@@ -1,3 +1,3 @@
-RELEASEVERSION=1.1.5
+RELEASEVERSION=1.1.7
 
 java -Djava.library.path=. -jar sudokuki-"$RELEASEVERSION".jar -ui Swing
