1096d1e1a57d1a2624c6716376e98572142d69c6
svedrenne <sylvain@jankenpoi.net>
2014-04-06 15:57:32 +0200

    Updated make_jar_zip_release.sh

--- make_jar_zip_release.sh
+++ make_jar_zip_release.sh
@@ -6,7 +6,6 @@ make && make dist-zip
 mkdir -p $ZIPRELEASEDIR/
 
 cp NEWS README COPYING HOWTO_RUN_SUDOKUKI.txt $ZIPRELEASEDIR/
-cp libs/* $ZIPRELEASEDIR/
 cp sudokuki-$RELEASEVERSION.jar $ZIPRELEASEDIR/
 echo "java -Djava.library.path=. -jar sudokuki-"$RELEASEVERSION".jar -ui Swing" > $ZIPRELEASEDIR/sudokuki.sh
 chmod +x $ZIPRELEASEDIR"/sudokuki.sh"
